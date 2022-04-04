9a925fb658ffe69a82dfeec3da952515d1b1fb56
Ishaan <ishaan10dutta@gmail.com>
2022-04-04 16:03:07 +0530

    Post liking functionality added.

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -37,4 +37,11 @@ export const deletePost = (id) => async (dispatch) => {
   }
 }
 
-
+export const likePost = (id) => async (dispatch) => {
+  try {
+    const { data } = await api.likePost(id);
+    dispatch({ type: 'UPDATE', payload: data });
+  } catch (error) {
+    console.log(error);
+  }
+}
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -31,7 +31,7 @@ const Form = ({ currentId, setCurrentId}) => {
     }
 
     const clear=() => {
-        setCurrentId(null);
+        setCurrentId(0);
         setPostData({creator: '', title:'', message:'', tags:'', selectedFile:'' });
     }
 
@@ -57,7 +57,7 @@ const Form = ({ currentId, setCurrentId}) => {
                 <TextField name="tags" variant="outlined" 
                            label="Tags" fullWidth
                            value={postData.tags} 
-                           onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
+                           onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })}
                 />
                 <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })} /></div>
                 <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
